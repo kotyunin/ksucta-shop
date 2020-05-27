0343c19cd7bb14a7382555db146964f2e1c3e9da
Kirill <kotyunin.k@mail.ru>
2020-05-28 01:19:08 +0600

    Added gulp-sass plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const gulp = require('gulp'),
 			gp = require('gulp-load-plugins')();
 			
@@ -7,4 +9,11 @@ gulp.task('pug', () => {
 			pretty: true // disables html compressing
 		}))
 		.pipe(gulp.dest('build'));
+})
+
+gulp.task('scss', () => {
+	return gulp.src('src/static/scss/main.scss')
+		.pipe(gp.sass({}))
+		.pipe(gp.csso()) // Minimizing the code, removes duplicate styles
+		.pipe(gulp.dest('build/css'));
 })
