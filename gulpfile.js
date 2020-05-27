6146ebad82ce37e821ff56915c4e51a359225e17
Kirill <kotyunin.k@mail.ru>
2020-05-28 01:33:53 +0600

    Complemented gulp scss task

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,12 @@ gulp.task('pug', () => {
 
 gulp.task('scss', () => {
 	return gulp.src('src/static/scss/main.scss')
+		.pipe(gp.sourcemaps.init())
 		.pipe(gp.sass({}))
+		.on("error", gp.notify.onError({
+			title: 'Style'
+		}))
 		.pipe(gp.csso()) // Minimizing the code, removes duplicate styles
+		.pipe(gp.sourcemaps.write())
 		.pipe(gulp.dest('build/css'));
 })
