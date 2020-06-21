8e2d1f1b56ab413e37026058e664e42861501136
Kirill <kotyunin.k@mail.ru>
2020-06-21 23:22:15 +0600

    Fixed gulp task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ module.exports = () => {
   $.gulp.task('watch', () => {
     $.gulp.watch('src/pug/**/*.pug', $.gulp.series('pug'));
     $.gulp.watch('src/static/**/*.scss', $.gulp.series('scss'));
-    $.gulp.watch('src/static/scritps/main.js', $.gulp.series('scripts'));
+    $.gulp.watch('src/static/scripts/main.js', $.gulp.series('scripts'));
     $.gulp.watch('src/static/images/*', $.gulp.series('img:dev'));
   })
 }
